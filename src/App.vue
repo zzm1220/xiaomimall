f280e2ff9461dcc6d4b3d6bb3719dadb201158c4
zhaozhimin <zhaozhimin@chinaiptv.cn>
2020-01-07 16:33:05 +0800

    项目目录结构搭建

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+  },
+  data() {
+    return {
+      age: 29,
+      result: ''
+    }
+  },
+  mounted() {
   }
 }
 </script>
